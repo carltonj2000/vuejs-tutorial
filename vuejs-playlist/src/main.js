4a092221864263aa84f3aea3cd6a3280c7903fd4
Carlton Joseph <carlton.joseph@gmail.com>
2017-07-18 02:51:15 -0700

    add code for component and included it globally and locally

--- vuejs-playlist/src/main.js
+++ vuejs-playlist/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// next two lines for global nesting
+//import Ninjas from './Ninjas.vue'
+//Vue.component('ninjas', Ninjas)
+
 new Vue({
   el: '#app',
   render: h => h(App)
